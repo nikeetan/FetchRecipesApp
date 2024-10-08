720c38b816b8b792b00389940c8f3f671875ec1b
Niketan Doddamani <niketandoddamani@gmail.com>
2024-10-08 13:02:08 -0500

    Initial Commit

---
+++ Fetch Recipes/Fetch_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesApp.swift
+//  Fetch Recipes
+//
+//  Created by Niketan Doddamani on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch RecipesTests/Fetch_RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipesTests.swift
+//  Fetch RecipesTests
+//
+//  Created by Niketan Doddamani on 10/8/24.
+//
+
+import Testing
+@testable import Fetch_Recipes
+
+struct Fetch_RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
